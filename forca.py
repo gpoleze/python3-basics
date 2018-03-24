ed14239dd2db601e752edf297f012b695de0ae6b
Gabriel Ferreira <gabriel@jostle.me>
2018-03-23 22:54:09 -0700

    File at the end of chapter 3 - lists

--- forca.py
+++ forca.py
@@ -18,6 +18,27 @@ def jogar():
 
     cabecalho()
 
+    palavra_secreta = "banana"
+
+    letras_acertadas = ["_" for letra in palavra_secreta]
+    print(letras_acertadas)
+
+    enforcou = False
+    acertou = False
+
+    while (not enforcou and not acertou):
+
+        chute = input("Qual letra? ")
+        chute = chute.strip()
+
+        index = 0
+        for letra in palavra_secreta:
+            if (chute.upper() == letra.upper()):
+                letras_acertadas[index] = letra
+            index = index + 1
+
+        print(letras_acertadas)
+
     fim_de_jogo(pontuacao)
 
 
